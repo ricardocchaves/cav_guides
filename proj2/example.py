7d1c45d761e83bbc9d52ef102ba8f2ab2949485d
Ricardo Chaves <ricardochaves@ua.pt>
2019-10-27 20:15:19 +0000

    Testing bistring package.

--- proj2/example.py
+++ proj2/example.py
@@ -4,5 +4,34 @@ sys.path.append('..') # Making the other packages in the repository visible."
 import proj1.imgpy as img
 import proj1.audiopy as audio
 
-print("Running")
-img.show('../fotos/kodim03.png')
+def main():
+    print("Running")
+    #img.show('../fotos/kodim03.png')
+
+def testingBitstring():
+    from bitstring import BitArray
+    """
+    ## Creating BitArray
+    # from a binary string
+    a = BitArray('0b001')
+    # from a hexadecimal string
+    b = BitArray('0xff470001')
+    # straight from a file
+    c = BitArray(filename='somefile.ext')
+    # from an integer
+    d = BitArray(int=540, length=11)
+    # using a format string
+    d = BitArray('int:11=540')
+    """
+    a = BitArray('0xff01')
+    b = BitArray('0b11001000')
+    print(b.bin)
+    print(b.hex)
+    print(b.bytes)
+    print(b.len)
+    print(b.length)
+    print(b+b)
+    print(a+(b+b))
+
+if __name__ == "__main__":
+    testingBitstring()

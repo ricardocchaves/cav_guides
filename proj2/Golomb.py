94e50e0aa7287d363c8774a105712b725a671978
Ricardo Chaves <ricardochaves@ua.pt>
2019-11-03 02:14:10 +0000

    Fixed Golomb, now working correctly. Added decoding function.

--- proj2/Golomb.py
+++ proj2/Golomb.py
@@ -1,34 +1,58 @@
-
-import math 
-from bitstring import BitArray
+import math
 
 class Golomb():
-  
-    value = 0
-    length = 0
-
+    # Encode into Golomb
+    # Val - int
+    # m   - int
     @classmethod
     def to_golomb(cls, val, m):
-        
-        q = val//m
-        r = val%m
+        c = int(math.ceil(math.log(m,2)))
+        r = val % m
+        q =int(math.floor(val / m))
+        div = int(math.pow(2,c) - m)
+        unary = '1'*q
 
-        if( (m & (m-1) == 0) and m != 0): 
-            #power of 2
-            r_binstr = "{0:b}".format(r)
+        if (r < div):
+            b = c - 1
+            binary = "{0:0" + str(b) + "b}"
+            binary = binary.format(r)
         else:
-            b = math.ceil(math.log2(m))
-            x = r + 2**b - m
-            
-            r_binstr = "{0:b}".format(x)
+            b = c
+            binary = "{0:0" + str(b) + "b}"
+            binary = binary.format(r+div)
 
-        q_binstr = Golomb.to_unarystr(q)
-
-        print(r_binstr)
-        print(q_binstr)
-
-        return q_binstr + r_binstr
+        golomb = unary + "0" +str(binary)
+        return golomb
 
     @classmethod 
-    def to_unarystr(cls, val):
+    def _to_unarystr(cls, val):
         return '1'*val + '0'
+
+    @classmethod
+    def _from_unary(cls, val):
+        return len(val[:-1])
+
+    # Decode from Golomb
+    # val - string
+    @classmethod
+    def from_golomb(cls, val, m):
+        c = int(math.ceil(math.log(m,2)))
+        div = int(math.pow(2,c) - m)
+        pos = 0
+        for bit in val:
+            if bit == '0':
+                break
+            else:
+                pos+=1
+        q = pos
+        r = int(val[pos+1:],2)
+        if r > div:
+            r = r - div
+        return q*m + r
+
+if __name__ == "__main__":
+    for i in range(15+1):
+        g = Golomb.to_golomb(i,5)
+        print(g)
+        print(Golomb.from_golomb(g,5))
+        print("----")

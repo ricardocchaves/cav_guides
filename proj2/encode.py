e3ef234a631de97bfdf644ffc725985bc0b5ff27
Hugo Sampaio <hsampaio@ua.pt>
2019-11-17 12:59:45 +0000

    [feat] Adds encode.py as main program with every encoding stage
    Needs final stage -> calling a toGolomb like function

---
+++ proj2/encode.py
@@ -0,0 +1,194 @@
+import wave
+import struct
+import sys
+from Golomb import Golomb
+from tqdm import tqdm
+from bitstring import BitArray
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+import operator
+
+from scipy.optimize import curve_fit
+
+
+def readWavFile(fname):
+    sound = wave.open(fname, "r")
+
+    buffer = BitArray()
+
+    # File attributes
+    n_channels = sound.getnchannels()
+    samp_width = sound.getsampwidth()
+    framerate = sound.getframerate()
+
+    prev_left = 0
+    prev_right = 0
+    sample_values = []
+    diff_values = []
+    
+    for _ in tqdm(range(sound.getnframes())):
+        sample = sound.readframes(1)
+        valLeft, valRight = struct.unpack('<hh', sample)
+    
+        sample_values += [(valLeft,valRight)]
+
+        d_left = valLeft - prev_left
+        d_right = valRight - prev_right
+
+        # Updating last difference
+        prev_left = valLeft
+        prev_right = valRight
+
+        diff_values += [(d_left,d_right)]
+
+    return sample_values, diff_values
+
+
+def prob(data):
+
+    d = {}
+    samples = 0
+    for value in data:
+        if value in d:
+            v = d.get(value)
+            d.update({value: v+1 })
+        else:
+            d.update({value: 1})
+        samples += 1
+    for k in d:
+        d.update({k: d.get(k)/samples})
+    
+    return d
+    #d = prob de cada sample, tranformed = prob de cada simbolo, mapping = simbolo de cada sample
+    #mapeament está "aleatório"
+
+def map_symbols( sample_probability ):
+
+    sorted_samples = sorted(sample_probability.items(), key=operator.itemgetter(1), reverse=True)
+    #sample from most likely to least
+
+    transformed = {}
+    symbol_mapping = {}
+    i = 0
+    for k in sorted_samples:
+        symbol_mapping.update({i: k})
+        i += 1
+
+    return symbol_mapping
+
+def findBestGolomb(symbol_mapping, show):
+    x = [k for k in symbol_mapping.keys()]
+    y = [symbol_mapping.get(k)[1] for k in symbol_mapping.keys()]
+
+    popt, pcov = curve_fit(golomb_pattern, x, y)
+
+    if show:
+        plt.figure(figsize=(7, 5))
+        plt.plot( x, golomb_pattern(x, *popt), 'bo', markersize=0.5)
+        plt.ylabel('curve')
+        plt.rcParams['agg.path.chunksize'] = 10000
+        #plt.axis([1,len(samples)+1, -2**15, 2**15])
+        plt.show(block = False)
+
+        plt.plot( x, y, 'ro', markersize=0.5)
+        plt.ylabel('probability')
+        plt.rcParams['agg.path.chunksize'] = 10000
+        #plt.axis([1,len(samples)+1, -2**15, 2**15])
+        plt.show(block = False)
+
+    alpha = popt[0]
+    m_param = math.ceil( -1/math.log2(alpha) )
+
+    return alpha, m_param
+
+def golomb_pattern(x, alpha):
+    return (alpha**x)*(1-alpha)
+
+def main():
+    if len(sys.argv) < 3:
+        print("USAGE: python3 {} sound_file.wav show".format(sys.argv[0]))
+        return
+    fname = sys.argv[1]
+    show = sys.argv[2]
+
+#1 #Read Samples from file
+    samples, diff_samples = readWavFile(fname)
+    #print(samples)
+
+
+#2 #Calculate probabilty of each sample value after predictor is applyed
+    sample_probability = prob([r for (l,r) in diff_samples])
+
+    #show sample values' probability distribution after predictor is applyed
+    if(show):
+        plt.figure(figsize=(7, 5))
+        plt.plot( [k for k in sample_probability.keys()], [sample_probability.get(k) for k in sample_probability.keys()], 'ro', markersize=0.5)
+        plt.ylabel('probability')
+        plt.rcParams['agg.path.chunksize'] = 10000
+        #plt.axis([1,len(samples)+1, -2**15, 2**15])
+        plt.show(block = False)
+
+    
+#3 #Map symbols to sample values accourding to probability
+    #{ symbol: (value, probability)}
+    symbol_mapping = map_symbols(sample_probability)
+
+    #show choosen symbols' probability distribution 
+    if(show):
+        plt.figure(figsize=(7, 5))
+        plt.plot( [s for s in symbol_mapping.keys()], [symbol_mapping.get(s)[1] for s in symbol_mapping.keys()], 'ro', markersize=0.5)
+        plt.ylabel('probability')
+        #plt.axis([1,len(samples)+1, -2**15, 2**15])
+        plt.show(block = False)
+    
+#4 #Find best Golomb Parameters
+    alpha, m_param = findBestGolomb(symbol_mapping,show)
+    print("Found Parameters: \n Alpha: {}, \n M: {} ".format(alpha,m_param))
+
+#5 #Encode as Golomb
+    #TODO
+
+    if show:
+        input("Press enter to exit")
+
+    
+
+    
+
+    # #Golomb pattern
+    # plt.figure(figsize=(7, 5))
+    # plt.plot( [x for x in range(0, 100)], [golomb_pattern(x,0.5) for x in range(0,100)], 'ro', markersize=0.5)
+    # plt.ylabel('probability')
+    # plt.rcParams['agg.path.chunksize'] = 10000
+    # #plt.axis([1,len(samples)+1, -2**15, 2**15])
+    # plt.show(block = True)
+
+    
+    '''
+    plt.figure(figsize=(10, 10))
+    plt.plot( [ s+1 for s in range(0,len(samples))], [r for (l,r) in samples])
+    plt.ylabel('some numbers')
+    plt.rcParams['agg.path.chunksize'] = 10000
+    plt.axis([1,len(samples)+1, -2**15, 2**15])
+    plt.show(block = False)
+
+    plt.figure(figsize=(10, 10))
+    x = [r for (l,r) in diff_samples]
+    plt.hist(x, density=True)
+    plt.ylabel('Probability')
+    plt.show(block = False)
+    
+    plt.figure(figsize=(10, 10))
+    plt.plot( [ s+1 for s in range(0,len(diff_samples))], [r for (l,r) in diff_samples])
+    plt.rcParams['agg.path.chunksize'] = 10000
+    #plt.axis([1,len(samples)+1, -2**15, 2**15])
+    plt.show()
+    '''
+    
+
+
+if __name__ == "__main__":
+    main()
+

e9c815009021355123a65bb91a0d4b73ea8bddde
Ricardo Chaves <ricardochaves@ua.pt>
2019-11-22 15:20:53 +0000

    [feat] Starts of project 3. Basic video player.

---
+++ proj3/player.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import sys
+import math
+
+def main():
+	if (len(sys.argv) < 2):
+		print("USAGE: python3 {} videoFile".format(sys.argv[0]))
+		return
+
+	fname = sys.argv[1]
+	cap = cv.VideoCapture(fname)
+	framerate = math.ceil(cap.get(cv.CAP_PROP_FPS))
+	
+	if(not cap.isOpened()):
+		print("Error opening video!")
+		return
+
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+		if ret:
+			cv.imshow('Frame',frame)
+			if cv.waitKey(framerate) & 0xFF == ord('q'):
+				break
+		else:
+			break
+
+if __name__ == "__main__":
+	main()

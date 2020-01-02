84daa38a4f58593612bfc05fa1f3027518a8db49
Ricardo Chaves <ricardochaves@ua.pt>
2020-01-02 16:17:20 +0000

    [doc] Adds comments with guidelines for codec 1.

--- proj3/codec_1.py
+++ proj3/codec_1.py
@@ -3,7 +3,7 @@ import sys
 import math
 import numpy as np
 from tqdm import tqdm
-from multiprocessing import Process, Value
+from multiprocessing import Process, Value, Manager
 import time
 from pixel_iteration import pixel_iteration_fast
 
@@ -30,18 +30,18 @@ def main():
 		elif arg == "-decode":
 			decode(inputFile,outFile)
 		
+"""
+m_y,m_u,m_v = m
+c_y = int(math.ceil(math.log(m_y,2)))
+div_y = int(math.pow(2,c_y) - m_y)
+c_u = int(math.ceil(math.log(m_u,2)))
+div_u = int(math.pow(2,c_u) - m_u)
+c_v = int(math.ceil(math.log(m_v,2)))
+div_v = int(math.pow(2,c_v) - m_v)
+"""
+
 def encode(inputFile, outputFile, m=5):
 	cap = VideoCaptureYUV(inputFile)
-
-	"""
-	m_y,m_u,m_v = m
-	c_y = int(math.ceil(math.log(m_y,2)))
-	div_y = int(math.pow(2,c_y) - m_y)
-	c_u = int(math.ceil(math.log(m_u,2)))
-	div_u = int(math.pow(2,c_u) - m_u)
-	c_v = int(math.ceil(math.log(m_v,2)))
-	div_v = int(math.pow(2,c_v) - m_v)
-	"""
 
 	f = open(outputFile, 'wb')
 	header = cap.header.encode('utf-8')
@@ -49,6 +49,8 @@ def encode(inputFile, outputFile, m=5):
 
 	cnt = 1
 	current = Value('i',0)
+	#manager = multiprocessing.Manager()
+    #return_dict = manager.dict()
 	pbar = tqdm(total=500)
 	while True:
 		ret, frame = cap.read_raw()
@@ -73,7 +75,7 @@ def encode(inputFile, outputFile, m=5):
 			"""
 			
 			while current.value == 4:
-				time.sleep(0.1)
+				time.sleep(0.1) # because 'wait' doesn't exist
 			p = Process(target=processFrame,args=(cap.height,cap.width,frame,f,m,cnt,current))
 			p.start()
 			cnt += 1
@@ -81,16 +83,30 @@ def encode(inputFile, outputFile, m=5):
 			current.value += 1
 		else:
 			break
+	# After all 500 frames:
+	# Find best 'm' for every value in each overall
+	# np.array(500*size(overall)*3)
+	# [val,val,val,val,val,val,...] use frame_len to separate frames
+	# for valueToEncode in np.array:
+	#	outputBuffer = Golomb.toGolomb(value)
+	# Write metadata and encoded data to output file
 	f.close()
 
 def processFrame(height,width,frame,handler,m,ID,current):
-	#print("Started {}".format(ID))
+	# for pixel:
+	# 	Predicted value
+	# 	pixel value
+	#	value to encode
+	#	y_overall += valueToEncode.y
+	#	u_overall += valueToEncode.u
+	#	v_overall += valueToEncode.v
 	s = time.time()
-	outFrame = pixel_iteration_slow(height,width,frame)
-	#outFrame = np.array(pixel_iteration_fast(height,width,frame))
+	#outFrame = pixel_iteration_slow(height,width,frame)
+	outFrame = np.array(pixel_iteration_fast(height,width,frame))
 	writeFrame(handler,outFrame,m)
 	#print(time.time()-s)
 	current.value -= 1
+	# Add y,u,v overall to globalOverall (y,u and v) dict/list in the frame position
 
 def pixel_iteration_slow(height,width,frame):
 	outFrame = np.copy(frame)
